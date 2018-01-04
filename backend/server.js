8875ca5fc117c916282b91b3688fa854462394bc
jeshsy <jeshsy@users.noreply.github.com>
2018-01-04 15:07:42 -0500

    connected the api routes + controller to the server

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ const express  = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const app = express();
+const { routes } = require('./api/routes/routes');
 
 const port = process.env.PORT || '3000';
 app.set('port', port);
 
 const server = http.createServer(app);
+
+routes(app);
 
 server.listen(port, () => console.log(`Running on path: ${port}`));
