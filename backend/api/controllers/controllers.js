e3b362c1287ded792d57004987e9563d1915ae2d
jeshsy <jeshsy@users.noreply.github.com>
2018-01-04 16:00:53 -0500

    implemented Axios to clean up routes

--- backend/api/controllers/controllers.js
+++ backend/api/controllers/controllers.js
@@ -1,24 +1,32 @@
 const http = require("http");
+const axios = require('axios');
 
 const test = (req, res) => {
   res.json({ msg: 'online' });
 };
 
 const testget = (req, res) => {
-  let rawData = '';
-  http.get('http://www.recipepuppy.com/api/?i=sugar', (res) => {
-    res.setEncoding('utf8');
-    res.on('data', (chunk) => { rawData += chunk; });
-    res.on('end', () => {
-      try {
-        const parsedData = JSON.parse(rawData);
-        console.log(parsedData.results);
-      } catch (e) {
-        console.error(e.message);
-      }
-    });
-  }).on('error', (e) => {
-    console.error(`Got error: ${e.message}`);
+  // let rawData = '';
+  // http.get('http://www.recipepuppy.com/api/?i=sugar', (res) => {
+  //   res.setEncoding('utf8');
+  //   res.on('data', (chunk) => { rawData += chunk; });
+  //   res.on('end', () => {
+  //     try {
+  //       const parsedData = JSON.parse(rawData);
+  //       console.log(parsedData.results);
+  //     } catch (e) {
+  //       console.error(e.message);
+  //     }
+  //   });
+  // }).on('error', (e) => {
+  //   console.error(`Got error: ${e.message}`);
+  // });
+  // res.json(rawData);
+  axios({
+    method:'get',
+    url:'http://www.recipepuppy.com/api/?i=sugar',
+  }).then((res) => {
+    console.log(res.data);
   });
 };
 
