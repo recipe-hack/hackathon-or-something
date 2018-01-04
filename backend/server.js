28c5e558ce4d361913ded97eb7519d1befab2444
jeshsy <jeshsy@users.noreply.github.com>
2018-01-04 16:00:24 -0500

    added CORS and bodyparser

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,28 @@
 const express  = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const { routes } = require('./api/routes/routes');
 
+const corsOptions = {
+  'origin': 'http://localhost:3000',
+  'methods': 'GET, HEAD, PUT, PATCH, POST, DELETE',
+  'preflightContinue': true,
+  'optionsSuccessStatus': 204,
+  'credentials': true // enable set cookie
+};
+
 const port = process.env.PORT || '3000';
+
 app.set('port', port);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
+routes(app);
 
 const server = http.createServer(app);
 
-routes(app);
+
 
 server.listen(port, () => console.log(`Running on path: ${port}`));
