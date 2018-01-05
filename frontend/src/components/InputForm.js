d7c30b7182f050b725f9b43dcafb314eedb4657c
Troy Williams <troywilliams@Troys-MBP.domain>
2018-01-04 20:56:17 -0600

    axios calling in the submit form. Search working food and ingredient type

--- frontend/src/components/InputForm.js
+++ frontend/src/components/InputForm.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-//const axios = require('axios');
+import React, {Component} from 'react';
+import RecipeCard from './recipeCard/recipeCard';
+const axios = require('axios');
 // import './InputForm.css';
 
 class InputForm extends Component {
@@ -8,43 +9,52 @@ class InputForm extends Component {
     this.state = {
       ingredients: '',
       food: '',
+      recipes: []
     };
 
     this.submit = this.submit.bind(this);
   }
 
-  submit() {
-    const { ingredients, food } = this.state;
-    this.setState({
-      ingredients: '',
-      food: '',
-    })
+  submit(e) {
+    e.preventDefault();
+
+    const {ingredients, food} = this.state;
+    return axios({method: 'get', url: `http://localhost:5000/api/recipes/?ingredients=${this.state.ingredients}&food=${this.state.food}&p=2`}).then((results) => {
+      console.log(results.data.results)
+      return results.data.results;
+    }).then((data) => {
+      let recipes = data.map((recipe, index) => {
+        return (<div className='col-sm-4' key={index}>
+          <div className="card">
+            <img className="card-img-top" src={recipe.thumbnail || 'http://img.recipepuppy.com/9.jpg'} alt={recipe.title}></img>
+            <div className="card-body">
+              <a className='btn-primary btn' href={recipe.href} target="_blank">View Recipe</a>
+            </div>
+          </div>
+        </div>)
+      });
+      this.setState({recipes: recipes});
+      this.setState({ingredients: '', food: ''})
+    }).catch((err) => {
+      console.log(err);
+    });
   }
 
   render() {
-    return(
-    <form onSubmit={this.submit}>
-      <input
-        type='text'
-        value={this.state.ingredients}
-        style={{ marginRight: '5px' }}
-        placeholder='ingredients'
-        onChange={event => this.setState({ ingredients: event.target.value })}
-      >
-      </input>
-      <input
-        type='text'
-        value={this.state.food}
-        style={{ marginRight: '5px' }}
-        placeholder='food'
-        onChange={event => this.setState({ food: event.target.value })}
-      >
-      </input>
-      <button>
-        Submit
-      </button>
-    </form>
-    )
+    return (<div>
+      <form onSubmit={this.submit}>
+        <input type='text' value={this.state.ingredients} style={{
+            marginRight: '5px'
+          }} placeholder='ingredients' onChange={event => this.setState({ingredients: event.target.value})}></input>
+        <input type='text' value={this.state.food} style={{
+            marginRight: '5px'
+          }} placeholder='food' onChange={event => this.setState({food: event.target.value})}></input>
+        <button>
+          Submit
+        </button>
+      </form>
+      <RecipeCard recipes={this.state.recipes}/>
+    </div>)
   }
 }
 
--- frontend/src/components/recipeCard/recipeCard.js
+++ frontend/src/components/recipeCard/recipeCard.js
@@ -1,46 +1,17 @@
-import React, { Component } from 'react';
-import'./recipeCard.css';
-const axios = require('axios');
-
+import React, {Component} from 'react';
+import './recipeCard.css';
+//const axios = require('axios');
 
 class RecipeCard extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       recipes: []
     };
   }
 
-  componentWillMount() {
-    return axios({
-      method: 'get',
-      url:
-      'http://localhost:5000/api/recipes/?ingredients=peas&food=salad&p=1'})
-    .then((results) => {
-      return results.data.results;
-    }).then((data) => {
-      let recipes = data.map((recipe, index) => {
-        return (
-          <div className='col-sm-4' key={index}>
-            <div className="card">
-              <img className="card-img-top" src={recipe.thumbnail || 'http://img.recipepuppy.com/9.jpg'} alt={recipe.title}></img>
-                <div className="card-body">
-                  <a className='btn-primary btn' href={recipe.href} target="_blank">View Recipe</a>
-                </div>
-            </div>
-      </div>
-          )
-      });
-      this.setState({recipes: recipes});
-    }).catch((err) => {
-      console.log(err);
-    });
-  }
-
   render() {
-    return (
-      <div>{this.state.recipes}</div>
-      );
+    return (<div>{this.props.recipes}</div>);
   }
 }
 
