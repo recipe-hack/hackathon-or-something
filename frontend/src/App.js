7e1f3195b64114ff6dd42075c34db46c4200a0a7
Adam Fetters <adamfetterscoder@gmail.com>
2018-01-05 08:55:05 -0500

    fixed location of button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import InputForm from './components/InputForm';
 import { Container } from 'react-bootstrap';
 
 class App extends Component {
-  render() {
-    return (<div className="App container-fluid">
-      <header className="App-header">
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
+  render () {
+    return (
+      <div className='App container-fluid'>
+        <header className='App-header'>
+          <h1 className='App-title'>Hackathon Recipe Search</h1>
+          <h3 className='App-details'>
+            Input ingredients, food, both or neither, or just leave blank for
+            random lookup
+          </h3>
+        </header>
 
-        <InputForm/>
-    </div>);
+        <InputForm />
+      </div>
+    );
   }
 }
 
--- frontend/src/components/InputForm.js
+++ frontend/src/components/InputForm.js
@@ -1,12 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import RecipeCard from './recipeCard/recipeCard';
 import Favorites from './Favorites/Favorites';
 import './InputForm.css';
 const axios = require('axios');
 
-
 class InputForm extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
     this.state = {
       ingredients: '',
@@ -25,149 +24,264 @@ class InputForm extends Component {
     this.favorite = this.favorite.bind(this);
   }
 
-  favorite(recipe) {
-    return() => {
+  favorite (recipe) {
+    return () => {
       let newArr = this.state.favorites;
       newArr.push(recipe);
-      this.setState({favorites: newArr});
-      console.log(this.state.favorites);
-        let newFavoritesArray = this.state.favorites.map((recipe, index) => {
-          return (
-            <div className='col-md-4' key={index}>
-            <div className="card">
-              <img className="card-img-top" src={recipe.thumbnail || 'http://img.recipepuppy.com/9.jpg'} alt={recipe.title}></img>
-              <div className="card-body">
-                <div className="">{recipe.title}</div>
-                <div>Ingredients: {recipe.ingredients}</div>
-                <a className='btn-primary btn' href={recipe.href} target="_blank">View Recipe</a>
+      this.setState({
+        favorites: newArr,
+        favorited: true
+      });
+
+      let newFavoritesArray = this.state.favorites.map((recipe, index) => {
+        return (
+          <div className='col-md-4' key={index}>
+            <div className='card'>
+              <img
+                className='card-img-top'
+                src={recipe.thumbnail || 'http://img.recipepuppy.com/9.jpg'}
+                alt={recipe.title}
+              />
+              <div className='card-body'>
+                <div className=''>
+                  {recipe.title}
+                </div>
+                <div>
+                  Ingredients: {recipe.ingredients}
+                </div>
+                <a
+                  className='btn-primary btn'
+                  href={recipe.href}
+                  target='_blank'
+                >
+                  View Recipe
+                </a>
               </div>
             </div>
-          </div>)
-        })
-        this.setState({favorites: newFavoritesArray});
-        console.log(`input 2  ${this.state.favorites}`)
-      }
+          </div>
+        );
+      });
+      this.setState({
+        favorites: newFavoritesArray
+      });
+    };
+  }
+
+  submit (e) {
+    e.preventDefault();
+    const { ingredients, food } = this.state;
+    return axios({
+      method: 'get',
+      url: `http://localhost:5000/api/recipes/?ingredients=${this.state
+        .ingredients}&food=${this.state.food}&p=${this.state.page}`
+    })
+      .then(results => {
+        return results.data.results;
+      })
+      .then(data => {
+        let recipes = data.map((recipe, index) => {
+          return (
+            <div className='col-md-4' key={index}>
+              <div className='card'>
+                <img
+                  className='card-img-top'
+                  src={recipe.thumbnail || 'http://img.recipepuppy.com/9.jpg'}
+                  alt={recipe.title}
+                />
+                <div className='card-body'>
+                  <div className=''>
+                    {recipe.title}
+                  </div>
+                  <div>
+                    Ingredients: {recipe.ingredients}
+                  </div>
+                  <a
+                    className='btn-primary btn'
+                    href={recipe.href}
+                    target='_blank'
+                  >
+                    View Recipe
+                  </a>
+                  <button
+                    className='btn btn-primary'
+                    onClick={this.favorite({ recipe })}
+                  >
+                    favorite
+                  </button>
+                </div>
+              </div>
+            </div>
+          );
+        });
+        this.setState({ recipes: recipes });
+        this.setState({
+          oldingredients: this.state.ingredients,
+          oldfood: this.state.food,
+          oldpage: this.state.page
+        });
+        this.setState({ ingredients: '', food: '', page: 1 });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  PrevPage (e) {
+    e.preventDefault();
+    let tempPage = this.state.oldpage;
+    if (tempPage > 1) {
+      let tempPage = this.state.oldpage - 1;
+      this.setState({ oldpage: tempPage });
     }
-
-
-  submit(e) {
-    e.preventDefault();
-    const {ingredients, food} = this.state;
-    return axios({method: 'get', url: `http://localhost:5000/api/recipes/?ingredients=${this.state.ingredients}&food=${this.state.food}&p=${this.state.page}`}).then((results) => {
-      return results.data.results;
-    }).then((data) => {
-      let recipes = data.map((recipe, index) => {
-        return (<div className='col-md-4' key={index}>
-          <div className="card">
-            <img className="card-img-top" src={recipe.thumbnail || 'http://img.recipepuppy.com/9.jpg'} alt={recipe.title}></img>
-            <div className="card-body">
-              <div className="">{recipe.title}</div>
-              <div>Ingredients: {recipe.ingredients}</div>
-              <a className='btn-primary btn' href={recipe.href} target="_blank">View Recipe</a>
-              <button className='btn btn-primary' onClick={this.favorite({recipe})}>favorite</button>
+    return axios({
+      method: 'get',
+      url: `http://localhost:5000/api/recipes/?ingredients=${this.state
+        .oldingredients}&food=${this.state.oldfood}&p=${this.state.oldpage}`
+    })
+      .then(results => {
+        return results.data.results;
+      })
+      .then(data => {
+        let recipes = data.map((recipe, index) => {
+          return (
+            <div className='col-md-4' key={index}>
+              <div className='card'>
+                <img
+                  className='card-img-top'
+                  src={recipe.thumbnail || 'http://img.recipepuppy.com/9.jpg'}
+                  alt={recipe.title}
+                />
+                <div className='card-body'>
+                  <div className=''>
+                    {recipe.title}
+                  </div>
+                  <div>
+                    Ingredients: {recipe.ingredients}
+                  </div>
+                  <a
+                    className='btn-primary btn'
+                    href={recipe.href}
+                    target='_blank'
+                  >
+                    View Recipe
+                  </a>
+                  <button
+                    className='btn btn-primary'
+                    onClick={this.favorite({ recipe })}
+                  >
+                    favorite
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>)
+          );
+        });
+        this.setState({ recipes: recipes });
+      })
+      .catch(err => {
+        console.log(err);
       });
-      this.setState({recipes: recipes});
-      this.setState({oldingredients: this.state.ingredients, oldfood: this.state.food, oldpage: this.state.page,})
-      this.setState({ingredients: '', food: '', page: 1,});
-    }).catch((err) => {
-      console.log(err);
-    });
   }
 
-  PrevPage(e) {
-    e.preventDefault();
-    let tempPage = this.state.oldpage;
-      if(tempPage > 1) {
-        let tempPage = this.state.oldpage - 1;
-        this.setState({oldpage: tempPage,});
-      }
-    return axios({method: 'get', url: `http://localhost:5000/api/recipes/?ingredients=${this.state.oldingredients}&food=${this.state.oldfood}&p=${this.state.oldpage}`}).then((results) => {
-      return results.data.results;
-    }).then((data) => {
-      let recipes = data.map((recipe, index) => {
-        return (<div className='col-md-4' key={index}>
-          <div className="card">
-            <img className="card-img-top" src={recipe.thumbnail || 'http://img.recipepuppy.com/9.jpg'} alt={recipe.title}></img>
-            <div className="card-body">
-              <div className="">{recipe.title}</div>
-              <div>Ingredients: {recipe.ingredients}</div>
-              <a className='btn-primary btn' href={recipe.href} target="_blank">View Recipe</a>
-              <button className='btn btn-primary' onClick={this.favorite({recipe})}>favorite</button>
-            </div>
-          </div>
-        </div>)
-      });
-      this.setState({recipes: recipes});
-    }).catch((err) => {
-      console.log(err);
-    });
-  }
-
-  NextPage(e) {
+  NextPage (e) {
     e.preventDefault();
     let tempPage = this.state.oldpage;
-      if(tempPage >= 1) {
-        let tempPage = this.state.oldpage + 1;
-        this.setState({oldpage: tempPage,});
-      }
-    return axios({method: 'get', url: `http://localhost:5000/api/recipes/?ingredients=${this.state.oldingredients}&food=${this.state.oldfood}&p=${this.state.oldpage}`}).then((results) => {
-      return results.data.results;
-    }).then((data) => {
-      let recipes = data.map((recipe, index) => {
-        return (<div className='col-md-4' key={index}>
-          <div className="card">
-            <img className="card-img-top" src={recipe.thumbnail || 'http://img.recipepuppy.com/9.jpg'} alt={recipe.title}></img>
-            <div className="card-body">
-              <div className="">{recipe.title}</div>
-              <div>Ingredients: {recipe.ingredients}</div>
-              <a className='btn-primary btn' href={recipe.href} target="_blank">View Recipe</a>
-              <button className='btn btn-primary' onClick={this.favorite({recipe})}>favorite</button>
+    if (tempPage >= 1) {
+      let tempPage = this.state.oldpage + 1;
+      this.setState({ oldpage: tempPage });
+    }
+    return axios({
+      method: 'get',
+      url: `http://localhost:5000/api/recipes/?ingredients=${this.state
+        .oldingredients}&food=${this.state.oldfood}&p=${this.state.oldpage}`
+    })
+      .then(results => {
+        return results.data.results;
+      })
+      .then(data => {
+        let recipes = data.map((recipe, index) => {
+          return (
+            <div className='col-md-4' key={index}>
+              <div className='card'>
+                <img
+                  className='card-img-top'
+                  src={recipe.thumbnail || 'http://img.recipepuppy.com/9.jpg'}
+                  alt={recipe.title}
+                />
+                <div className='card-body'>
+                  <div className=''>
+                    {recipe.title}
+                  </div>
+                  <div>
+                    Ingredients: {recipe.ingredients}
+                  </div>
+                  <a
+                    className='btn-primary btn'
+                    href={recipe.href}
+                    target='_blank'
+                  >
+                    View Recipe
+                  </a>
+                  <button
+                    className='btn btn-primary'
+                    onClick={this.favorite({ recipe })}
+                  >
+                    favorite
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>)
+          );
+        });
+        this.setState({ recipes: recipes });
+      })
+      .catch(err => {
+        console.log(err);
       });
-      this.setState({recipes: recipes});
-    }).catch((err) => {
-      console.log(err);
-    });
   }
 
-
-  render() {
-    return (<div className='row'>
-      <div className='col-md-12'>
-        <form onSubmit={this.submit}>
-          <input type='text' value={this.state.ingredients} style={{
-              marginRight: '5px'
-            }} placeholder='ingredients' onChange={event => this.setState({ingredients: event.target.value})}></input>
-          <input type='text' value={this.state.food} style={{
-              marginRight: '5px'
-            }} placeholder='food' onChange={event => this.setState({food: event.target.value})}></input>
-          <button className='btn btn-primary'>
-            Submit
-          </button>
-        </form>
-      </div>
-      <div className='col-md-12'>
-        <div>
-          <RecipeCard className='row' recipes={this.state.recipes}/>
-          <form onSubmit={this.PrevPage}>
-            <button className='btn btn-primary'>
-              Previous Page
-            </button>
+  render () {
+    return (
+      <div className='row'>
+        <div className='col-md-12'>
+          <form onSubmit={this.submit}>
+            <input
+              type='text'
+              value={this.state.ingredients}
+              style={{
+                marginRight: '5px'
+              }}
+              placeholder='ingredients'
+              onChange={event =>
+                this.setState({ ingredients: event.target.value })}
+            />
+            <input
+              type='text'
+              value={this.state.food}
+              style={{
+                marginRight: '5px'
+              }}
+              placeholder='food'
+              onChange={event => this.setState({ food: event.target.value })}
+            />
+            <button className='btn btn-primary'>Submit</button>
           </form>
-          <form onSubmit={this.NextPage}>
-            <button className='btn btn-primary'>
-              Next Page
-            </button>
-          </form>
-          <Favorites favorites={this.state.favorites}/>
+        </div>
+        <div className='col-md-12'>
+          <div>
+            <RecipeCard className='row' recipes={this.state.recipes} />
+            <div class='page-buttons'>
+              <form onSubmit={this.PrevPage}>
+                <button className='btn btn-primary'>Previous Page</button>
+              </form>
+              <form onSubmit={this.NextPage}>
+                <button className='btn btn-primary'>Next Page</button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
-    </div>)
+    );
   }
 }
 
