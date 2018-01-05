0b6d94f6f8445df4522cf71c26c0f5bb4bf5e1d2
jeshsy <jeshsy@users.noreply.github.com>
2018-01-05 01:04:30 -0500

    basic css

--- frontend/src/components/recipeCard/recipeCard.js
+++ frontend/src/components/recipeCard/recipeCard.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+// import CSS files
 import './recipeCard.css';
 //const axios = require('axios');
 
@@ -11,7 +12,11 @@ class RecipeCard extends Component {
   }
 
   render() {
-    return (<div>{this.props.recipes}</div>);
+    // for css you just put things in the classname
+    // delete these comments btw
+    // extraclass can be used as a class as well. just add any classes
+    // after any bootstrap classes you've added
+    return (<div className= 'test extraclass'/*just put the classname you want in here*/>{this.props.recipes}</div>);
   }
 }
 
